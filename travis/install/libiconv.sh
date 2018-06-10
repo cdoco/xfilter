3938aa8a39e6fab3cb9649077c9eb2da8ea2276f
cdoco <gao_zihang@hotmail.com>
2018-06-10 20:07:48 +0900

    Fix bugs

--- travis/install/libiconv.sh
+++ travis/install/libiconv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.7.tar.gz \
-&& tar zxvf libiconv-1.7.tar.gz \
-&& cd libiconv-1.7 && ./configure && make && sudo make install
+wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.4.tar.gz \
+&& tar zxvf libiconv-1.4.tar.gz \
+&& cd libiconv-1.4 && ./configure && make && sudo make install
