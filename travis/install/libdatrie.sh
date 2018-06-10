3c6c4afff71aab2ac8d49562ea3b8fc6c2547d0d
cdoco <gao_zihang@hotmail.com>
2018-06-10 20:21:35 +0900

    Fix bugs

--- travis/install/libdatrie.sh
+++ travis/install/libdatrie.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/tlwg/libdatrie/releases/download/v0.2.11/libdatrie-0.2.11.tar.xz \
 && tar xvf libdatrie-0.2.11.tar.xz \
-&& cd libdatrie-0.2.11 && ./configure ./configure LDFLAGS=-L/usr/local/lib LIBS=-liconv && make && make install
+&& cd libdatrie-0.2.11 && ./configure LDFLAGS=-L/usr/local/lib LIBS=-liconv && make && make install
