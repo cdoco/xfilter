a0bd44dc162dec2aac6c8879a31606a60be42081
cdoco <gao_zihang@hotmail.com>
2018-06-10 16:06:31 +0900

    Repair Chinese search problem

--- xfilter.c
+++ xfilter.c
@@ -31,16 +31,21 @@ zend_class_entry *xfilter_ce;
 
 ZEND_DECLARE_MODULE_GLOBALS(xfilter)
 
+PHP_INI_BEGIN()
+    STD_PHP_INI_ENTRY("xfilter.filename", NULL, PHP_INI_ALL, OnUpdateString, filename, zend_xfilter_globals, xfilter_globals)
+PHP_INI_END()
+
+/* Xfilter search */
 static int xfilter_search(Trie *trie, const AlphaChar *text, zval *data)
 {
-	TrieState *s = NULL;
-	const AlphaChar *p = NULL;
-	const AlphaChar *base = NULL;
+    TrieState *s = NULL;
+    const AlphaChar *p = NULL;
+    const AlphaChar *base = NULL;
     zval word;
 
-	base = text;
+    base = text;
     if (!(s = trie_root(trie))) {
-		return FAILURE;
+        return FAILURE;
     }
 
     while (*text) {
@@ -65,184 +70,237 @@ static int xfilter_search(Trie *trie, const AlphaChar *text, zval *data)
     }
     trie_state_free(s);
 
-	return SUCCESS;
+    return SUCCESS;
 }
 
-/* Construct. */
-PHP_METHOD(xfilter, __construct)
+/* Trie new ex */
+static Trie *xfilter_trie_new_ex()
 {
-	zend_string *filename = NULL;
+    Trie *trie;
+    AlphaMap *alpha_map;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "S", &filename) == FAILURE) {
-        return;
-    }
-
-	if (ZSTR_LEN(filename) < 1 || strlen(ZSTR_VAL(filename)) != ZSTR_LEN(filename)) {
-        php_error_docref(NULL, E_WARNING, "need to set the file path");
-        RETURN_FALSE;
-    }
-
-	XFILTER_G(filename) = ZSTR_VAL(filename);
-	RETURN_TRUE;
-}
-
-/* Save function */
-PHP_METHOD(xfilter, save)
-{
-	zend_ulong i;
-	zval *dirty_words = NULL, *value = NULL;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "a", &dirty_words) == FAILURE) {
-        return;
-    }
-
-    ZEND_HASH_FOREACH_NUM_KEY_VAL(Z_ARRVAL_P(dirty_words), i, value) {
-		AlphaChar alpha_key[KEYWORD_MAX_LEN+1];
-		unsigned char *p = NULL;
-		size_t i = 0;
-
-		if (Z_TYPE_P(value) == IS_STRING) {
-			/* Test length problem */
-			if (Z_STRLEN_P(value) > KEYWORD_MAX_LEN || Z_STRLEN_P(value) < 1) {
-				php_error_docref(NULL, E_WARNING, "keyword should has [1, %d] bytes", KEYWORD_MAX_LEN);
-				RETURN_FALSE;
-			}
-
-			p = (unsigned char *)Z_STRVAL_P(value);
-
-    		while (*p && *p != '\n' && *p != '\r') {
-        		alpha_key[i++] = (AlphaChar)*p;
-        		p++;
-    		}
-
-    		alpha_key[i] = TRIE_CHAR_TERM;
-    		if (!trie_store(XFILTER_G(trie), alpha_key, -1)) {
-        		RETURN_FALSE;
-    		}
-		} else {
-			php_error_docref(NULL, E_WARNING, "keyword type should be string");
-		}
-    } ZEND_HASH_FOREACH_END();
-
-	/* save file */
-	if (trie_save(XFILTER_G(trie), XFILTER_G(filename))) {
-        RETURN_FALSE;
-    }
-
-	RETURN_TRUE;
-}
-
-/* Search function */
-PHP_METHOD(xfilter, search)
-{
-	zend_string *text = NULL;
-	AlphaChar *alpha_text = NULL;
-	size_t i;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "S", &text) == FAILURE) {
-        return;
-    }
-
-	if (ZSTR_LEN(text) < 1) {
-		php_error_docref(NULL, E_NOTICE, "input is empty");
-		return;
-	}
-
-	if (trie_is_dirty(XFILTER_G(trie))) {
-		XFILTER_G(trie) = trie_new_from_file(XFILTER_G(filename));
-		if (!XFILTER_G(trie)) {
-			php_error_docref(NULL, E_WARNING, "Unable to load %s", XFILTER_G(filename));
-			return;
-		}
-	}
-
-	array_init(return_value);
-	alpha_text = emalloc(sizeof(AlphaChar) * (ZSTR_LEN(text) + 1));
-
-	for (i = 0; i < ZSTR_LEN(text); i++) {
-		alpha_text[i] = (AlphaChar)ZSTR_VAL(text)[i];
-	}
-	alpha_text[ZSTR_LEN(text)] = TRIE_CHAR_TERM;
-
-	if (xfilter_search(XFILTER_G(trie), alpha_text, return_value)) {
-		/* handle */
-	}
-
-	efree(alpha_text);
-}
-
-static const zend_function_entry xfilter_methods[] = {
-    PHP_ME(xfilter, __construct, NULL, ZEND_ACC_CTOR | ZEND_ACC_PUBLIC)
-    PHP_ME(xfilter, save, NULL, ZEND_ACC_PUBLIC)
-	PHP_ME(xfilter, search, NULL, ZEND_ACC_PUBLIC)
-    PHP_FE_END
-};
-
-PHP_MINIT_FUNCTION(xfilter)
-{
-	AlphaMap *alpha_map;
-	zend_class_entry ce;
-    INIT_CLASS_ENTRY(ce, "Cdoco\\Filter", xfilter_methods);
-
-    xfilter_ce = zend_register_internal_class(&ce);
-
-	/* Init trie */
     alpha_map = alpha_map_new();
     if (!alpha_map) {
-        return FAILURE;
+        return NULL;
     }
 
     if (alpha_map_add_range(alpha_map, 0x00, 0xff) != 0) {
         /* treat all strings as byte stream */
         alpha_map_free(alpha_map);
-        return FAILURE;
+        return NULL;
     }
 
-    XFILTER_G(trie) = trie_new(alpha_map);
+    trie = trie_new(alpha_map);
     alpha_map_free(alpha_map);
 
-    if (!XFILTER_G(trie)) {      
+    if (!trie) {      
+        return NULL;
+    }
+
+    return trie;
+}
+
+/* Trie new */
+static int xfilter_trie_new()
+{
+    Trie *trie;
+
+    if (XFILTER_G(filename)) {
+        trie = trie_new_from_file(XFILTER_G(filename));
+        if (!trie) {
+            trie = xfilter_trie_new_ex();
+        }
+    } else {
+        trie = xfilter_trie_new_ex();
+    }
+
+    if (!trie) {
         return FAILURE;
     }
 
-	return SUCCESS;
+    XFILTER_G(trie) = trie;
+    return 0;
+}
+
+/* Construct. */
+PHP_METHOD(xfilter, __construct)
+{
+    zend_string *filename = NULL;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "|S", &filename) == FAILURE) {
+        return;
+    }
+
+    if (filename) {
+        if (ZSTR_LEN(filename) < 1 || strlen(ZSTR_VAL(filename)) != ZSTR_LEN(filename)) {
+            php_error_docref(NULL, E_WARNING, "need to set the file path");
+            RETURN_FALSE;
+        }
+
+        XFILTER_G(filename) = ZSTR_VAL(filename);
+
+        if (xfilter_trie_new()) {
+            RETURN_FALSE;
+        }
+    }
+
+    RETURN_TRUE;
+}
+
+/* Save function */
+PHP_METHOD(xfilter, save)
+{
+    zend_ulong i;
+    zend_bool append;
+    zval *dirty_words = NULL, *value = NULL;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "a|b", &dirty_words, &append) == FAILURE) {
+        return;
+    }
+
+	/* Init */
+    if (!append) {
+        XFILTER_G(trie) = xfilter_trie_new_ex();
+    }
+
+    ZEND_HASH_FOREACH_NUM_KEY_VAL(Z_ARRVAL_P(dirty_words), i, value) {
+        AlphaChar alpha_key[KEYWORD_MAX_LEN+1];
+        unsigned char *p = NULL;
+        size_t i = 0;
+
+        if (Z_TYPE_P(value) == IS_STRING) {
+            /* Test length problem */
+            if (Z_STRLEN_P(value) > KEYWORD_MAX_LEN || Z_STRLEN_P(value) < 1) {
+                php_error_docref(NULL, E_WARNING, "keyword should has [1, %d] bytes", KEYWORD_MAX_LEN);
+                RETURN_FALSE;
+            }
+
+            p = (unsigned char *)Z_STRVAL_P(value);
+
+            while (*p && *p != '\n' && *p != '\r') {
+                alpha_key[i++] = (AlphaChar)*p;
+                p++;
+            }
+
+            alpha_key[i] = TRIE_CHAR_TERM;
+            if (!trie_store(XFILTER_G(trie), alpha_key, -1)) {
+                RETURN_FALSE;
+            }
+        } else {
+            php_error_docref(NULL, E_WARNING, "keyword type should be string");
+        }
+    } ZEND_HASH_FOREACH_END();
+
+    /* save file */
+    if (trie_save(XFILTER_G(trie), XFILTER_G(filename))) {
+        RETURN_FALSE;
+    }
+
+    RETURN_TRUE;
+}
+
+/* Search function */
+PHP_METHOD(xfilter, search)
+{
+    AlphaChar *alpha_text = NULL;
+    unsigned char *text = NULL;
+    size_t i, text_len;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "s", &text, &text_len) == FAILURE) {
+        return;
+    }
+
+    if (text_len < 1) {
+        php_error_docref(NULL, E_NOTICE, "input is empty");
+        return;
+    }
+
+    if (trie_is_dirty(XFILTER_G(trie))) {
+        XFILTER_G(trie) = trie_new_from_file(XFILTER_G(filename));
+        if (!XFILTER_G(trie)) {
+            php_error_docref(NULL, E_WARNING, "Unable to load %s", XFILTER_G(filename));
+            return;
+        }
+    }
+
+    array_init(return_value);
+    alpha_text = emalloc(sizeof(AlphaChar) * (text_len + 1));
+
+    for (i = 0; i < text_len; i++) {
+        alpha_text[i] = (AlphaChar)text[i];
+    }
+    alpha_text[text_len] = TRIE_CHAR_TERM;
+
+    if (xfilter_search(XFILTER_G(trie), alpha_text, return_value)) {
+        /* handle */
+    }
+
+    efree(alpha_text);
+}
+
+static const zend_function_entry xfilter_methods[] = {
+    PHP_ME(xfilter, __construct, NULL, ZEND_ACC_CTOR | ZEND_ACC_PUBLIC)
+    PHP_ME(xfilter, save, NULL, ZEND_ACC_PUBLIC)
+    PHP_ME(xfilter, search, NULL, ZEND_ACC_PUBLIC)
+    PHP_FE_END
+};
+
+PHP_MINIT_FUNCTION(xfilter)
+{
+    REGISTER_INI_ENTRIES();
+
+    zend_class_entry ce;
+    INIT_CLASS_ENTRY(ce, "Cdoco\\Filter", xfilter_methods);
+
+    xfilter_ce = zend_register_internal_class(&ce);
+
+    /* Init trie */
+    if (xfilter_trie_new()) {
+        return FAILURE;
+    }
+
+    return SUCCESS;
 }
 
 PHP_MSHUTDOWN_FUNCTION(xfilter)
 {
-	/* free */
-	if (XFILTER_G(trie)) {
-		trie_free(XFILTER_G(trie));
-	}
+    /* free */
+    UNREGISTER_INI_ENTRIES();
+
+    if (XFILTER_G(trie)) {
+        trie_free(XFILTER_G(trie));
+    }
 	
-	return SUCCESS;
+    return SUCCESS;
 }
 
 PHP_MINFO_FUNCTION(xfilter)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "xfilter support", "enabled");
-	php_info_print_table_end();
+    php_info_print_table_start();
+    php_info_print_table_header(2, "xfilter support", "enabled");
+    php_info_print_table_end();
+
+    DISPLAY_INI_ENTRIES();
 }
 
 /* GINIT */
 PHP_GINIT_FUNCTION(xfilter)
 {
     xfilter_globals->trie = NULL;
-	xfilter_globals->filename = NULL;
+    xfilter_globals->filename = NULL;
 }
 
 zend_module_entry xfilter_module_entry = {
-	STANDARD_MODULE_HEADER,
-	"xfilter",
-	NULL,
-	PHP_MINIT(xfilter),
-	PHP_MSHUTDOWN(xfilter),
-	NULL,
-	NULL,
-	PHP_MINFO(xfilter),
-	PHP_XFILTER_VERSION,
-	PHP_MODULE_GLOBALS(xfilter),
+    STANDARD_MODULE_HEADER,
+    "xfilter",
+    NULL,
+    PHP_MINIT(xfilter),
+    PHP_MSHUTDOWN(xfilter),
+    NULL,
+    NULL,
+    PHP_MINFO(xfilter),
+    PHP_XFILTER_VERSION,
+    PHP_MODULE_GLOBALS(xfilter),
     PHP_GINIT(xfilter),
     NULL,
     NULL,
--- xfilter.php
+++ xfilter.php
@@ -7,10 +7,25 @@ if(!extension_loaded('xfilter')) {
 
 use Cdoco\Filter;
 
-$fr = new Filter(__DIR__ . '/blackword.dic');
+$fr = new Filter(__DIR__ . '/blackword.dicxx');
 
-var_dump($fr->save(['word1', 'word2', 'word3']));
+$fr->save(['管理员','admin','你妈'], true);
 
-print_r($fr->search('hello word2 word1'));
+$strContent = '你妈打撒杀死大家阿斯看管理得到顶管理员顶顶顶的dfdfkjdfkjkjdfjrtrtiayuewfjdjsjdfssakcvgadminretyyu速度思考坑';
+
+echo "start memory=".memory_get_usage(true)."\n";date_default_timezone_set('Asia/Chongqing');
+$test = array('a', 'abd', 'dad', 'admin', 'admin', 'word1f', 'cword1', 'cword1t');
+foreach ($test as $v) {
+	for ($i = 0;$i <= 10000; $i++) {
+		$fr->search($v);
+	}
+}
+
+echo "end memory=".memory_get_usage(true)."\n";
+echo date('Y-m-d H:i:s');
+
+// foreach ($arrRet as $v) {
+// 	echo substr($strContent, $v[0], $v[1]) . "\n";
+// }
 
 ?>
