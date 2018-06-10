836050d39d35d9b39c85ed88e82a360b9ceeb967
cdoco <gao_zihang@hotmail.com>
2018-06-10 19:34:32 +0900

    Modify the grammar mistake

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -3,7 +3,6 @@
 wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.7.tar.gz \
 && tar zxvf libiconv-1.7.tar.gz \
 && cd libiconv-1.7 && ./configure && make && sudo make install && cd .. \
-
 && wget https://github.com/tlwg/libdatrie/releases/download/v0.2.11/libdatrie-0.2.11.tar.xz \
 && tar zxvf libdatrie-0.2.11.tar.xz \
 && cd libdatrie-0.2.11 && ./configure && make && make install
